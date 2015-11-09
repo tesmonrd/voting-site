c329213136f9b8899035cdd12e73d5646fbbc3a4
Rick Tesmond <tesmonrd@gmail.com>
2015-11-09 15:53:55 -0800

    Working gif appearance, need to correct rep and get gifs all running

--- voting.js
+++ voting.js
@@ -1,3 +1,13 @@
+
+var photoCollection = [];
+
+
+function Photo(name,path) {
+  this.name = name;
+  this.path = path;
+  photoCollection.push(this);
+}
+
 var cheeseDrake = new Photo ("Skee-ball is Drakes real passion", "images/cheesedrake.gif");
 var doubleDance = new Photo ("Carlton wants in on the action", "images/doubledance.gif");
 var fruitNinja = new Photo ("FruitNinja game going 100", "images/fruit.gif");
@@ -11,13 +21,16 @@ var takeOnMe = new Photo ("Drake's moves trancend decades", "images/takeonme.gif
 var tennisPro = new Photo ("He's like Verdasco on steroids", "images/tennis.gif");
 var trumpDance = new Photo ("Even Trump can't stop Hotline Bling", "images/trump.gif");
 
-var photoCollection = [];
+console.log(cheeseDrake);
+var pickPhoto1 = function() {
+  var leftPic = Math.floor(Math.random() * photoCollection.length);
+  document.getElementById("pictureOne").src = photoCollection[leftPic].path;
+};
+var pickPhoto2 = function() {
+  var rightPic = Math.floor(Math.random() * photoCollection.length);
+  document.getElementById("pictureTwo").src = photoCollection[rightPic].path;
+};
 
-var function Photo(name,path) = {
-  this.name = name;
-  this.path = path;
-  photoCollection.push(this);
-
-  for(i=0; i<length.photoCollection; i++)
-    document.getElementById(pictureOne)
-}
+Photo();
+pickPhoto1();
+pickPhoto2();
