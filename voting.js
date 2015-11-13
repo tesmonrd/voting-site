cf31d6e2c597195e601eabafc88cfbf906d7c088
Rick Tesmond <tesmonrd@gmail.com>
2015-11-13 14:18:24 -0800

    For real working this time, stored and update photoColletion in event

--- voting.js
+++ voting.js
@@ -9,44 +9,33 @@ function Photo(name,path,caption) {
   photoCollection.push(this);
 }
 
-var cheeseDrake = new Photo ("Chuck-E Cheese", "images/cheesedrake.gif","Skee-ball is Drakes real passion");
-var doubleDance = new Photo ("Carlton + Drake", "images/doubledance.gif", "Carlton wants in on the action");
-var fruitNinja = new Photo ("FruitNinja Drake","images/fruit.gif", "FruitNinja game going 100");
-var gassyDrake = new Photo ("Gassy Drake", "images/gassy.gif", "Truth");
-var lightSaber = new Photo ("Jedi Drake", "images/lightsaber.gif", "The Force is strong in this one");
-var pizzaChef = new Photo ("Pizza Drake", "images/pizza.gif", "Drake is the original Papa John");
-var pokeDrake = new Photo ("Pokedrake", "images/pokedrake.gif", "Catchin' Em All");
-var hanSolo = new Photo ("Deflecting Drake", "images/shoot.gif", "Its a TRAP!");
-var holoDrake = new Photo ("Hologram Drake", "images/starwars.gif", "His moves are our final hope.");
-var takeOnMe = new Photo ("Take On Drake", "images/takeonme.gif", "Drake's moves trancend decades");
-var tennisPro = new Photo ("Tennis Drake", "images/tennis.gif", "He's like Verdasco on steroids");
-var trumpDance = new Photo ("Hotline Trump", "images/trump.gif", "Even Trump can't stop Hotline Bling");
 var rightPic;
 var leftPic;
 var pictureOne = document.getElementById("pictureOne");
 var pictureTwo = document.getElementById("pictureTwo");
 var chartName = [];
-
-// ++++++Photo&Caption Display+++++++//
-var pickPhoto1 = function() {
-  leftPic = Math.floor(Math.random() * photoCollection.length);
-  pictureOne.src = photoCollection[leftPic].path;
-  document.getElementById("capOne").innerHTML = photoCollection[leftPic].caption;
-};
-
-var pickPhoto2 = function() {
-  do {
-    rightPic = Math.floor(Math.random() * photoCollection.length);
-  } while(leftPic === rightPic)
-  pictureTwo.src = photoCollection[rightPic].path;
-  document.getElementById("capTwo").innerHTML = photoCollection[rightPic].caption;
-};
+var button1 = document.getElementById('button1');
+var button2 = document.getElementById('button2');
 
 function checkLocal() {
-  if(localStorage.storeData) {
+  if(localStorage.storeData && localStorage.photoCollection) {
     data = JSON.parse(localStorage.storeData);
+    photoCollection = JSON.parse(localStorage.photoCollection);
     console.log(data);
   } else {
+    var cheeseDrake = new Photo ("Chuck-E Cheese", "images/cheesedrake.gif","Skee-ball is Drakes real passion");
+    var doubleDance = new Photo ("Carlton + Drake", "images/doubledance.gif", "Carlton wants in on the action");
+    var fruitNinja = new Photo ("FruitNinja Drake","images/fruit.gif", "FruitNinja game going 100");
+    var gassyDrake = new Photo ("Gassy Drake", "images/gassy.gif", "Truth");
+    var lightSaber = new Photo ("Jedi Drake", "images/lightsaber.gif", "The Force is strong in this one");
+    var pizzaChef = new Photo ("Pizza Drake", "images/pizza.gif", "Drake is the original Papa John");
+    var pokeDrake = new Photo ("Pokedrake", "images/pokedrake.gif", "Catchin' Em All");
+    var hanSolo = new Photo ("Deflecting Drake", "images/shoot.gif", "Its a TRAP!");
+    var holoDrake = new Photo ("Hologram Drake", "images/starwars.gif", "His moves are our final hope.");
+    var takeOnMe = new Photo ("Take On Drake", "images/takeonme.gif", "Drake's moves trancend decades");
+    var tennisPro = new Photo ("Tennis Drake", "images/tennis.gif", "He's like Verdasco on steroids");
+    var trumpDance = new Photo ("Hotline Trump", "images/trump.gif", "Even Trump can't stop Hotline Bling");
+    localStorage.setItem('photoCollection', JSON.stringify(photoCollection));
     data = {
     labels : chartName,
     datasets : [
@@ -63,15 +52,29 @@ function checkLocal() {
      }
 }; checkLocal();
 
-// ++++++ButtonEVENT+++++ //
-var button1 = document.getElementById('button1');
-var button2 = document.getElementById('button2');
+// ++++++Photo&Caption Display+++++++//
+var pickPhoto1 = function() {
+  leftPic = Math.floor(Math.random() * photoCollection.length);
+  pictureOne.src = photoCollection[leftPic].path;
+  document.getElementById("capOne").innerHTML = photoCollection[leftPic].caption;
+};
 
+var pickPhoto2 = function() {
+  do {
+    rightPic = Math.floor(Math.random() * photoCollection.length);
+  } while(leftPic === rightPic)
+    pictureTwo.src = photoCollection[rightPic].path;
+    document.getElementById("capTwo").innerHTML = photoCollection[rightPic].caption;
+};
+
+// ++++++ButtonEVENT+++++ //
 var addPoints1 = function() {
   photoCollection[leftPic].votes++;
+  console.log(photoCollection[leftPic] + " " + photoCollection[leftPic].votes);
   data.datasets[0].data[leftPic] = photoCollection[leftPic].votes;
   myBarChart.datasets[0].bars[leftPic].value = photoCollection[leftPic].votes;
   localStorage.setItem('storeData',JSON.stringify(data));
+  localStorage.setItem('photoCollection', JSON.stringify(photoCollection));
   resetPictures();
   myBarChart.update();
 };
@@ -80,6 +83,7 @@ var addPoints2 = function(){
   data.datasets[0].data[rightPic] = photoCollection[rightPic].votes;
   myBarChart.datasets[0].bars[rightPic].value = photoCollection[rightPic].votes;
   localStorage.setItem('storeData',JSON.stringify(data));
+  localStorage.setItem('photoCollection', JSON.stringify(photoCollection));
   resetPictures();
   myBarChart.update();
 };
